0dc1ea2d865ffa379a55f43747f03d095f22b19c
Josephina Bos <josephinabos@gmail.com>
2018-02-28 11:49:45 +0100

    adjust procfile

--- surfly-crm-integration.py
+++ surfly-crm-integration.py
@@ -16,20 +16,20 @@ headers = {
 def index():
     render_template('index.html')
 
-def join():
-    # get the active session list
-    sessionid = request.form['sessionid']
-    request = Request('https://surfly.com/v2/sessions/?api_key=6dbf75022a294e9c9cb247bb142d9e76&active_session=true', headers=headers)
-    response_body = urlopen(request).read()
-    # this returns a json object, so you need to parse it before you can use the data inside
-    active_sessions = json.loads(response_body)
-    # iterate over the active session list
-    for key in active_sessions:
-    # if PIN that was filled in by agent equals a PIN from the active session list
-        if key == sessionid:
-    # here you grab the follower link from the json file and make sure that the users are redirected to it
-            follower = active_sessions["viewer_link"]
-    return redirect(follower)
+# def join():
+#     # get the active session list
+#     sessionid = request.form['sessionid']
+#     request = Request('https://surfly.com/v2/sessions/?api_key=6dbf75022a294e9c9cb247bb142d9e76&active_session=true', headers=headers)
+#     response_body = urlopen(request).read()
+#     # this returns a json object, so you need to parse it before you can use the data inside
+#     active_sessions = json.loads(response_body)
+#     # iterate over the active session list
+#     for key in active_sessions:
+#     # if PIN that was filled in by agent equals a PIN from the active session list
+#         if key == sessionid:
+#     # here you grab the follower link from the json file and make sure that the users are redirected to it
+#             follower = active_sessions["viewer_link"]
+#     return redirect(follower)
 
     # this:
 
